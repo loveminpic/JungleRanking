7e1f6d4bc4dff9b9a2f33831ae4a67617d264e68
kijen723 <kij2646@gmail.com>
2023-04-06 01:08:02 +0900

    edit - slack

--- slack.py
+++ slack.py
@@ -1,13 +1,10 @@
-import os
-from slack_sdk import WebClient 
 from slack_sdk.errors import SlackApiError
 from pymongo import MongoClient
 import requests
 from datetime import date
+client = MongoClient('localhost', 27017)
 
-client = MongoClient('localhost', 27017) 
 db = client.jranking
-
 cursor_all = db.users.find({})
 all_data = []
 for document in cursor_all:
@@ -16,11 +13,13 @@ all_data.sort(key=lambda x: x.get('total'),reverse=True)
 number_one = all_data[0]['name']
 starttime = date.today().strftime("%Y-%m-%d")
 time = str(starttime)
-
-token = "xoxb-5077704286977-5062165098101-cSJfUlyMj6clodM3RxDy7YaS"
+token = "xoxb-5077704286977-5062165098101-NAyggJaLDzTNZo8SBJiZ5jNK"
 channel = "#ranking-bot"
 text = time + " 의 공부시간 1위는 " + number_one
-
-requests.post("https://slack.com/api/chat.postMessage",
-    headers={"Authorization": "Bearer "+token},
-    data={"channel": channel,"text": text})
+try :
+    response = requests.post("https://slack.com/api/chat.postMessage",
+        headers={"Authorization": "Bearer "+token},
+        data={"channel": channel,"text": text})
+    print(response.text)
+except SlackApiError as e:
+    print("Error sending message: {}".format(e))
