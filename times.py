d8e90248e6b7724e0b4e66aea7ade622e4aa353c
kijen723 <kij2646@gmail.com>
2023-04-05 17:33:35 +0900

    edit function -times

--- times.py
+++ times.py
@@ -3,8 +3,6 @@ from pymongo import MongoClient
 import json
 from flask import Flask, render_template,request, make_response, flash, redirect
 from flask_jwt_extended import (JWTManager, create_access_token, create_refresh_token)
-import requests
-from bs4 import BeautifulSoup
 from datetime import datetime
 
 times_bp = Blueprint('times', __name__)
@@ -12,21 +10,21 @@ times_bp = Blueprint('times', __name__)
 client = MongoClient('localhost', 27017) 
 db = client.jranking
 
-@times_bp.route('/time/calculate', methods=['GET'])
+@times_bp.route('/time/calculate', methods=['POST', 'GET'])
 def timeCalculate():
     token = request.cookies.get('refresh_token')
-    headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-    data = requests.get('http://localhost:5000/studybutton',headers=headers)
-    soup = BeautifulSoup(data.text, 'html.parser')
-    # date = soup.select('#date')
-    starttime = soup.select('#starttime')
+    # date = request.form['date']
+    starttime = request.form['starttime']
+    starttimeformat = datetime.strptime(starttime, '%H:%M:%S')
     endtime = datetime.now()
     endtime = endtime.time().replace(microsecond=0)
-    caltime = (endtime - starttime)
+    endtime = str(endtime)
+    endtimeformat = datetime.strptime(endtime, '%H:%M:%S')
+    caltime = (endtimeformat - starttimeformat)
     user = db.users.find_one({'token':token})
-    totaltime = user['time'] + ((caltime.seconds)//60)
+    totaltime = user['total'] + ((caltime.seconds)//60)
     id = user['id']
     db.times.insert_one({'id':id, 'start':starttime, 'end':endtime})
     # db.times.insert_one({'id':id, 'date':date, 'start':starttime, 'end':endtime})
     db.users.update_one({'id':id},{'$set':{'total':totaltime}})
-    return render_template('date.html', endtime = endtime)
+    return render_template('date.html', starttime = starttime, endtime = endtime)
