403dc87d94bc60ab6d61173ae8a4341dd8fddac3
kijen723 <kij2646@gmail.com>
2023-04-05 20:02:59 +0900

    edit function - rank (no)

--- rank.py
+++ rank.py
@@ -8,57 +8,60 @@ from flask_jwt_extended import (
 rank_bp = Blueprint('rank', __name__)
 
 client = MongoClient('localhost', 27017) 
-db = client.jungle
+db = client.jranking
 
-
-@rank_bp.route("/rank", methods=['GET'])
+@rank_bp.route('/rank', methods=['POST', 'GET']) # 'GET'
 def rank():
     token = request.cookies.get('refresh_token')
     if token is not None :
-        users = list(db.users.find({}).sort({'total': -1}))
-        user = list(db.users.find_one({'token':token}))
-        users = json.dumps(users)
-        user = json.dumps(user)
-        return render_template('rank.html', ranker = users, user = user)
+        users = list(db.users.find({}, {'_id' : False}).sort('total', -1))
+        user = db.users.find_one({'token':token}, {'_id' : False})
+        users_give = []
+        for temp in users:
+            users_give.append(temp)
+        return render_template('rank.html', ranker = users_give, user = user)
     else :
         flash("로그인 정보가 없습니다.")
         return render_template('index.html')
 
-@rank_bp.route("/rank/red", methods=['GET'])
+@rank_bp.route('/rank', methods=['POST']) # 'GET'
 def red():
     token = request.cookies.get('refresh_token')
     if token is not None :
-        users = list(db.users.find({'classroom':'red'}).sort({'total': -1}))
-        user = list(db.users.find_one({'token':token}))
-        users = json.dumps(users)
-        user = json.dumps(user)
-        return render_template('rank.html', ranker = users, user = user)
-    else :
-        flash("로그인 정보가 없습니다.")
-        return render_template('index.html')
-    
-@rank_bp.route("/rank/blue", methods=['GET'])
-def blue():
-    token = request.cookies.get('refresh_token')
-    if token is not None :
-        users = list(db.users.find({'classroom':'blue'}).sort({'total': -1}))
-        user = list(db.users.find_one({'token':token}))
-        users = json.dumps(users)
-        user = json.dumps(user)
-        return render_template('rank.html', ranker = users, user = user)
+        users = list(db.users.find({'classroom':'레드반'}, {'_id' : False}).sort('total', -1))
+        user = db.users.find_one({'token':token}, {'_id' : False})
+        users_give = []
+        for temp in users:
+            users_give.append(temp)
+        return render_template('rank.html', ranker = users_give, user = user)
     else :
         flash("로그인 정보가 없습니다.")
         return render_template('index.html')
 
-@rank_bp.route("/rank/green", methods=['GET'])
+@rank_bp.route('/rank', methods=['POST']) # 'GET'
+def blue():
+    token = request.cookies.get('refresh_token')
+    if token is not None :
+        users = list(db.users.find({'classroom':'블루반'}, {'_id' : False}).sort('total', -1))
+        user = db.users.find_one({'token':token}, {'_id' : False})
+        users_give = []
+        for temp in users:
+            users_give.append(temp)
+        return render_template('rank.html', ranker = users_give, user = user)
+    else :
+        flash("로그인 정보가 없습니다.")
+        return render_template('index.html')
+
+@rank_bp.route('/rank', methods=['POST']) # 'GET'
 def green():
     token = request.cookies.get('refresh_token')
     if token is not None :
-        users = list(db.users.find({'classroom':'green'}).sort({'total': -1}))
-        user = list(db.users.find_one({'token':token}))
-        users = json.dumps(users)
-        user = json.dumps(user)
-        return render_template('rank.html', ranker = users, user = user)
+        users = list(db.users.find({'classroom':'그린반'}, {'_id' : False}).sort('total', -1))
+        user = db.users.find_one({'token':token}, {'_id' : False})
+        users_give = []
+        for temp in users:
+            users_give.append(temp)
+        return render_template('rank.html', ranker = users_give, user = user)
     else :
         flash("로그인 정보가 없습니다.")
         return render_template('index.html')
