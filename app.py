953a7de63c71c6a124edc49ba3e5ec965af5ca41
Minji Lee <minji0479@gmail.com>
2023-04-05 10:03:58 +0900

    signup connect with login page

--- app.py
+++ app.py
@@ -80,8 +80,45 @@ def login():
         # response.set_cookie(key="access_token", value=access_token, httponly=True)
         response.set_cookie('access_token', value=access_token)
         # response.localStorage.setItem("access_token", access_token)
-        
         return response
+    
+@app.route('/signupbutton', methods=['POST'])
+def signupbutton():
+    return render_template('signup.html')
+
+# 회원가입
+@app.route('/signup', methods=['POST'])
+def postUser():
+    if request.method == 'POST':
+        id = request.form['id']
+        password = request.form['password']
+        name = request.form['name']
+        classroom = request.form['classroom']
+        
+        # 이름 입력 확인
+        if name == '':
+            flash("이름을 입력해주세요!")
+            return render_template('signup.html')
+        
+        # id 중복 확인
+        temp = list(db.users.find({}))
+        for user in temp:
+            if id in user['id']:
+                flash("중복된 ID입니다!")
+                return render_template('signup.html')
+        
+        # pw 길이 확인
+        if len(password) < 8:
+            flash("8글자 이상의 비밀번호를 입력해주세요!")
+            return render_template('signup.html')
+        
+        # db에 저장
+        db.users.insert_one({'id':id, 'pw':password, 'name':name, 'classroom':classroom, 'total':0, 'token':''})
+        flash("가입이 완료되었습니다!")
+        return render_template('signup.html')
+    else:
+        return render_template('signup.html')
+
 
 @app.route("/study", methods=['POST'])
 def menu():
