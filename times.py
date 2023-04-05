6f21b86e9c67207df797efef9d46508ce9487456
kijen723 <kij2646@gmail.com>
2023-04-05 20:50:58 +0900

    edit function - times

--- times.py
+++ times.py
@@ -8,23 +8,29 @@ from datetime import datetime
 times_bp = Blueprint('times', __name__)
 
 client = MongoClient('localhost', 27017) 
-db = client.jungle
+db = client.jranking
 
 @times_bp.route('/time/calculate', methods=['POST', 'GET'])
 def timeCalculate():
+    temptime = '23:59:59'
+    temptime = datetime.strptime(temptime, '%H:%M:%S')
     token = request.cookies.get('refresh_token')
-    # date = request.form['date']
+    date = request.form['date']
     starttime = request.form['starttime']
     starttimeformat = datetime.strptime(starttime, '%H:%M:%S')
     endtime = datetime.now()
+    enddate = str(endtime.date())
     endtime = endtime.time().replace(microsecond=0)
     endtime = str(endtime)
     endtimeformat = datetime.strptime(endtime, '%H:%M:%S')
-    caltime = (endtimeformat - starttimeformat)
+    if date == enddate:
+        caltime = (endtimeformat - starttimeformat)
+    elif date < enddate:
+        caltime = (temptime - starttimeformat) + endtimeformat
     user = db.users.find_one({'token':token})
     totaltime = user['total'] + ((caltime.seconds)//60)
     id = user['id']
-    db.times.insert_one({'id':id, 'start':starttime, 'end':endtime})
-    # db.times.insert_one({'id':id, 'date':date, 'start':starttime, 'end':endtime})
+    # db.times.insert_one({'id':id, 'start':starttime, 'end':endtime})
+    db.times.insert_one({'id':id, 'date':date, 'start':starttime, 'end':endtime})
     db.users.update_one({'id':id},{'$set':{'total':totaltime}})
-    return render_template('date.html', starttime = starttime, endtime = endtime)
+    return render_template('date.html', starttime = starttime, endtime = endtime, date = date)
